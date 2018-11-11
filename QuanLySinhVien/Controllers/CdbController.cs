e2bc8160a40b071d246685f67c8394e239c7c7e8
quanghuy1242 <katygaga124@gmail.com>
2018-11-11 11:07:32 +0700

    Hided Connection String

--- QuanLySinhVien/Controllers/CdbController.cs
+++ QuanLySinhVien/Controllers/CdbController.cs
@@ -11,7 +11,7 @@ namespace QuanLySinhVien.Controllers
 {
     class CdbController
     {
-        private string connectionString = @"Data Source=.\sqlexpress;Initial Catalog=QuanLySinhVien2;Integrated Security=True";
+        private string connectionString = ConnectionString.LOCAL_STRING;
 
         public CdbController()
         {
